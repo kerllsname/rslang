8dabce8eed9e06e7c16ce505f41f4bc05b25fb21
Ihar Levachkov <pokerlis@gmail.com>
2022-02-05 19:21:08 +0300

    refactor: fix css style

---
+++ rslang/src/app/body/wrapper/main/main-text-box.ts
@@ -0,0 +1,20 @@
+import BaseComponent from '../../../utility/baseComponent';
+
+export default class MainBox {
+  readonly mainBox: HTMLElement;
+
+  constructor(private readonly root: HTMLElement, readonly text: string) {
+    this.mainBox = document.createElement('div');
+    this.text = text;
+  }
+
+  render(): HTMLElement {
+    this.root.appendChild(this.mainBox);
+    this.mainBox.classList.add('main-box');
+
+    new BaseComponent(this.mainBox, 'div', ['main__img']).render();
+    new BaseComponent(this.mainBox, 'p', ['main__text'], `${this.text}`).render();
+
+    return this.mainBox;
+  }
+}
