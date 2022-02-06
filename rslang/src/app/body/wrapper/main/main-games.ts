1a97f6301798e12611d45701926ac89bb2c16744
kerllsname <kirillpotapovnow@gmail.com>
2022-02-06 17:32:20 +0300

    fix: fix class names

--- rslang/src/app/body/wrapper/main/main-games.ts
+++ rslang/src/app/body/wrapper/main/main-games.ts
@@ -18,16 +18,16 @@ export default class MainGames {
     this.mainGames.classList.add('main__games');
 
     this.mainGames.appendChild(this.audioGame);
-    this.audioGame.classList.add('games__audio');
-    new BaseComponent(this.audioGame, 'div', ['games__audio-title'], 'audio challenge').render();
-    new BaseComponent(this.audioGame, 'div', ['games__audio-description'], 'audio challenge description').render();
-    new BaseComponent(this.audioGame, 'button', ['games__audio-play'], 'play').render();
+    this.audioGame.classList.add('main__games-audio');
+    new BaseComponent(this.audioGame, 'div', ['game-audio__title'], 'audio challenge').render();
+    new BaseComponent(this.audioGame, 'div', ['game-audio__description'], 'audio challenge description').render();
+    new BaseComponent(this.audioGame, 'button', ['game-audio__play'], 'play').render();
 
     this.mainGames.appendChild(this.sprintGame);
-    this.sprintGame.classList.add('games__sprint');
-    new BaseComponent(this.sprintGame, 'div', ['games__sprint-title'], 'sprint').render();
-    new BaseComponent(this.sprintGame, 'div', ['games__sprint-description'], 'sprint description').render();
-    new BaseComponent(this.sprintGame, 'button', ['games__sprint-play'], 'play').render();
+    this.sprintGame.classList.add('main__games-sprint');
+    new BaseComponent(this.sprintGame, 'div', ['game-sprint__title'], 'sprint').render();
+    new BaseComponent(this.sprintGame, 'div', ['game-sprint__description'], 'sprint description').render();
+    new BaseComponent(this.sprintGame, 'button', ['game-sprint__play'], 'play').render();
 
     return this.mainGames;
   }
