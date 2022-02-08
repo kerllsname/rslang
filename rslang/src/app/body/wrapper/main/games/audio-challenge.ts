0698c6e285161ca48d8333f527358e13f29bda1f
Ihar Levachkov <pokerlis@gmail.com>
2022-02-08 19:38:36 +0300

    feat: add layout of audio-challenge game

---
+++ rslang/src/app/body/wrapper/main/games/audio-challenge.ts
@@ -0,0 +1,31 @@
+import BaseComponent from '../../../../utility/base-сomponent';
+import AnswerBox from '../../../../utility/games/audio-challange/answer-box';
+
+export default class AudioChallange {
+  readonly audioChallange: HTMLElement;
+
+  readonly containerPanel: HTMLElement;
+
+  constructor(private readonly root: HTMLElement) {
+    this.audioChallange = document.createElement('div');
+    this.containerPanel = document.createElement('div');
+  }
+
+  render(): HTMLElement {
+    this.root.appendChild(this.audioChallange);
+    this.audioChallange.classList.add('main__games__audioChallange');
+
+    new BaseComponent(this.audioChallange, 'div', ['main__games__audioChallange-img']).render();
+    new BaseComponent(this.audioChallange, 'div', ['main__games__audioChallange-buttonSound']).render();
+    new BaseComponent(this.audioChallange, 'div', ['main__games__audioChallange-buttonSkip']).render();
+    new AnswerBox(this.audioChallange).render();
+
+    this.audioChallange.appendChild(this.containerPanel);
+    this.containerPanel.classList.add('main__games__audioChallange__containerPanel');
+    new BaseComponent(this.containerPanel, 'div', ['main__games__audioChallange__containerPanel-buttonVolume']).render();
+    new BaseComponent(this.containerPanel, 'div', ['main__games__audioChallange__containerPanel-counter'], '<span class="audioChallange__wordsCount">1</span>/<span class="audioChallange__wordsAll">20</span>').render();
+    new BaseComponent(this.containerPanel, 'div', ['main__games__audioChallange__containerPanel-fullScreen']).render();
+
+    return this.audioChallange;
+  }
+}
