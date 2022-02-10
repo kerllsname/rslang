87ab4a3bc129b579571d4e9bb9a7c1bfbdb766cc
Ihar Levachkov <pokerlis@gmail.com>
2022-02-10 21:35:03 +0300

    feat: add interfaces

---
+++ rslang/src/app/interface/word.ts
@@ -0,0 +1,15 @@
+export default interface IWord {
+  id: string,
+  page: number,
+  word: number,
+  image: string,
+  audio: string,
+  audioMeaning: string,
+  audioExample: string,
+  textMeaning: string,
+  textExample: string,
+  transcription: string,
+  wordTranslate: string,
+  textMeaningTranslate: string,
+  textExampleTranslate: string
+}
